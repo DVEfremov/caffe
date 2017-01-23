52b056b6c0e19d146501db72b1e34eb62f2923c7
Daniil Efremov <daniil.efremov@xored.com>
2017-01-23 11:12:42 +0700

    temporary commented for andoid compilation support

--- android/caffe_mobile.cpp
+++ android/caffe_mobile.cpp
@@ -55,28 +55,28 @@ CaffeMobile *CaffeMobile::Get(const string &model_path,
 }
 
 CaffeMobile::CaffeMobile(const string &model_path, const string &weights_path) {
-  CHECK_GT(model_path.size(), 0) << "Need a model definition to score.";
-  CHECK_GT(weights_path.size(), 0) << "Need model weights to score.";
-
-  Caffe::set_mode(Caffe::CPU);
-
-  clock_t t_start = clock();
-  net_.reset(new Net<float>(model_path, caffe::TEST));
-  net_->CopyTrainedLayersFrom(weights_path);
-  clock_t t_end = clock();
-  LOG(INFO) << "Loading time: " << 1000.0 * (t_end - t_start) / CLOCKS_PER_SEC
-            << " ms.";
-
-  CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
-  CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
-
-  Blob<float> *input_layer = net_->input_blobs()[0];
-  num_channels_ = input_layer->channels();
-  CHECK(num_channels_ == 3 || num_channels_ == 1)
-      << "Input layer should have 1 or 3 channels.";
-  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
-
-  scale_ = 0.0;
+//  CHECK_GT(model_path.size(), 0) << "Need a model definition to score.";
+//  CHECK_GT(weights_path.size(), 0) << "Need model weights to score.";
+//
+//  Caffe::set_mode(Caffe::CPU);
+//
+//  clock_t t_start = clock();
+//  net_.reset(new Net<float>(model_path, caffe::TEST));
+//  net_->CopyTrainedLayersFrom(weights_path);
+//  clock_t t_end = clock();
+//  LOG(INFO) << "Loading time: " << 1000.0 * (t_end - t_start) / CLOCKS_PER_SEC
+//            << " ms.";
+//
+//  CHECK_EQ(net_->num_inputs(), 1) << "Network should have exactly one input.";
+//  CHECK_EQ(net_->num_outputs(), 1) << "Network should have exactly one output.";
+//
+//  Blob<float> *input_layer = net_->input_blobs()[0];
+//  num_channels_ = input_layer->channels();
+//  CHECK(num_channels_ == 3 || num_channels_ == 1)
+//      << "Input layer should have 1 or 3 channels.";
+//  input_geometry_ = cv::Size(input_layer->width(), input_layer->height());
+//
+//  scale_ = 0.0;
 }
 
 CaffeMobile::~CaffeMobile() { net_.reset(); }
